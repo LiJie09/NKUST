053d2418d67302a0b9b0df44430899c0b8bd1a58
LinJie09 <zxc664231@gmail.com>
2021-12-14 00:10:08 +0800

    HW

--- Ptt_Spider.java
+++ Ptt_Spider.java
@@ -3,28 +3,27 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import javax.print.Doc;
+import java.io.IOException;
 
-public class Web_spider {
-    public static void main(String[] args) {
+public class Ptt_Spider {
+    public static void main(String[] args) throws IOException {
         try {
-            Document doc = Jsoup.connect("https://www.ptt.cc/bbs/").get();
-            Elements hotClasses = doc.select("#main-container .b-net a");
-            System.out.println(doc.title());
-            Elements newsHeadlines = doc.select("div.b-ent a");
+            Document doc = Jsoup.connect("https://www.ptt.cc/bbs/index.html").get();
+            Elements hotClasses = doc.select("#main-container .b-ent a");
             for (Element hotClass : hotClasses) {
-                String pttTitle = hotClasses.select(".board-name").text();
+                String pttTitle = hotClass.select(".board-name").text();
                 String articleSortDoc = hotClass.absUrl("href").toString();
                 Document hotBoardDoc = Jsoup.connect(articleSortDoc).get();
-                Elements articleSortClasses = hotBoardDoc.select("#main-container .r-ent.title a");
-                for (Element articleSortClass:articleSortClasses){
+                Elements articleSortClasses = hotBoardDoc.select("#main-container .r-ent .title a");
+                for (Element articleSortClass  : articleSortClasses){
                     String articleTitle = articleSortClass.text();
                     String articleDoc = articleSortClass.absUrl("href").toString();
-                    Document articleClasses = Jsoup.connect(articleSortDoc).get();
+                    Document articleClasses = Jsoup.connect(articleDoc).get();
+
                     if (!articleClasses.select(".push").isEmpty()){
                         Element firstPush = articleClasses.select("#main-container #main-content .push").get(0);
-                        String commitName = firstPush.select("span").get(1).text();
-                        String firstCommit = firstPush.select("span").get(2).text();
+                        String commitName = firstPush.select("span.f3.h1.push-userid ").text();
+                        String firstCommit = firstPush.select("span.f3.push-content ").text();
                         System.out.println("Board: "+ pttTitle);
                         System.out.println("Article Title:"+ articleTitle);
                         System.out.println("Commit"+ commitName + firstCommit);
